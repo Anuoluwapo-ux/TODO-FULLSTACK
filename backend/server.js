487b4fa7c7a03c3c8259b2d2d57a063ae3bd0e54
Anuoluwapo <classicwrld99@gmail.com>
2025-03-20 15:27:15 +0100

    Added some text

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 
+// const corsOptions ={
+//     origin:'http://localhost:3000', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
     res.json({ greetings: "hello wrld!!!!!" })
 });
